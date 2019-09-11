e04bad87c316c6a23c77b208f817a913d049881b
sarpisik <contact@sarpisik.com>
2019-09-11 22:11:58 +0300

    Updates

---
+++ precache-manifest.2dbca023f284c1eefb7e707fba6aea4d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d3c36f36b3cb1a47a51",
+    "url": "/soccer-club-spa/static/css/main.5807b640.chunk.css"
+  },
+  {
+    "revision": "0d3c36f36b3cb1a47a51",
+    "url": "/soccer-club-spa/static/js/main.20a740e6.chunk.js"
+  },
+  {
+    "revision": "b0952ceb91e560830066",
+    "url": "/soccer-club-spa/static/js/runtime~main.ee3ed51e.js"
+  },
+  {
+    "revision": "1a6e8708d39108e0893b",
+    "url": "/soccer-club-spa/static/js/2.e55b3b24.chunk.js"
+  },
+  {
+    "revision": "0a27edbc4e19ca78d07f",
+    "url": "/soccer-club-spa/static/js/3.6eacba19.chunk.js"
+  },
+  {
+    "revision": "c8ac34bab7e68e8325e1",
+    "url": "/soccer-club-spa/static/js/4.896d6210.chunk.js"
+  },
+  {
+    "revision": "2816253cdf64f6beaded",
+    "url": "/soccer-club-spa/static/js/5.bec03694.chunk.js"
+  },
+  {
+    "revision": "78bd7f219edfcc1bc666",
+    "url": "/soccer-club-spa/static/js/6.12902640.chunk.js"
+  },
+  {
+    "revision": "46e4edc5b5dc758d4a42",
+    "url": "/soccer-club-spa/static/js/7.04b43749.chunk.js"
+  },
+  {
+    "revision": "bd0ea5a8d0d3da5c3687",
+    "url": "/soccer-club-spa/static/js/8.7a56c5bb.chunk.js"
+  },
+  {
+    "revision": "a84d8e12f22421de1ea04f3641c5f238",
+    "url": "/soccer-club-spa/static/media/Muğlaspor.a84d8e12.png"
+  },
+  {
+    "revision": "178910ad6c132642f61dcae1bc6e4221",
+    "url": "/soccer-club-spa/static/media/1.178910ad.jpeg"
+  },
+  {
+    "revision": "0ed1891a7b556080c3877ab617a66c2a",
+    "url": "/soccer-club-spa/static/media/10.0ed1891a.jpeg"
+  },
+  {
+    "revision": "9d3948f6158ccd157a88eacd1ff0a735",
+    "url": "/soccer-club-spa/static/media/2.9d3948f6.jpeg"
+  },
+  {
+    "revision": "347a441d88bee406aecb065993f75dac",
+    "url": "/soccer-club-spa/static/media/3.347a441d.jpeg"
+  },
+  {
+    "revision": "d6f48180bce5a7f412ef0d324709e23a",
+    "url": "/soccer-club-spa/static/media/4.d6f48180.jpg"
+  },
+  {
+    "revision": "2878b5344eb45aeb2e2e41273ccaa2d4",
+    "url": "/soccer-club-spa/static/media/5.2878b534.jpg"
+  },
+  {
+    "revision": "f5d9a25b1a3ed7c5b9149aa38ee4a09a",
+    "url": "/soccer-club-spa/static/media/6.f5d9a25b.jpg"
+  },
+  {
+    "revision": "07bd2e9931da69d086a6a8583c65bb62",
+    "url": "/soccer-club-spa/static/media/7.07bd2e99.jpeg"
+  },
+  {
+    "revision": "888939a7d1c2f1689350fc793ddab1eb",
+    "url": "/soccer-club-spa/static/media/8.888939a7.jpeg"
+  },
+  {
+    "revision": "a51fc52fc1cbfa7453a050d48db692da",
+    "url": "/soccer-club-spa/static/media/9.a51fc52f.jpeg"
+  },
+  {
+    "revision": "0e800529ab9d5cc071545875ed6e017a",
+    "url": "/soccer-club-spa/static/media/pexels-photo-1277213.0e800529.jpeg"
+  },
+  {
+    "revision": "1261b30be80803effb3bb54c7bbf431c",
+    "url": "/soccer-club-spa/static/media/pexels-photo-262524.1261b30b.jpeg"
+  },
+  {
+    "revision": "4135fb34c632af8b4f945f43df89a569",
+    "url": "/soccer-club-spa/static/media/pexels-photo-41257.4135fb34.jpeg"
+  },
+  {
+    "revision": "97a4ea7dfbb6c8e1af43d746602e437c",
+    "url": "/soccer-club-spa/static/media/pexels-photo-918798.97a4ea7d.jpeg"
+  },
+  {
+    "revision": "bec1bf58f1443c077ef9b4e909326ac5",
+    "url": "/soccer-club-spa/static/media/fake_logo.bec1bf58.png"
+  },
+  {
+    "revision": "7bd4dbdddf4b31e9469428dd3a8b6325",
+    "url": "/soccer-club-spa/static/media/business-779542_640.7bd4dbdd.jpg"
+  },
+  {
+    "revision": "b83d9f708d63939d6ea201033d86ce63",
+    "url": "/soccer-club-spa/static/media/d-1935811_640.b83d9f70.png"
+  },
+  {
+    "revision": "a4370619feeab62f71ab643dace34b1e",
+    "url": "/soccer-club-spa/static/media/design-751452_640.a4370619.png"
+  },
+  {
+    "revision": "dc76b779248730cb0cb8f01dc3a289ce",
+    "url": "/soccer-club-spa/static/media/lion-715852_640.dc76b779.png"
+  },
+  {
+    "revision": "1f2ed44e0cce136f29ed5814ceff1ffc",
+    "url": "/soccer-club-spa/static/media/star-2822548_640.1f2ed44e.png"
+  },
+  {
+    "revision": "8e5f272284bdfa14316a9e9ff34d14df",
+    "url": "/soccer-club-spa/static/media/vector-2534950_640.8e5f2722.png"
+  },
+  {
+    "revision": "ced76caf8e102636139055e85a7e8825",
+    "url": "/soccer-club-spa/static/media/uniform1.ced76caf.png"
+  },
+  {
+    "revision": "97a371765aecd68ba3fb15dba317b4ce",
+    "url": "/soccer-club-spa/static/media/uniform2.97a37176.png"
+  },
+  {
+    "revision": "6393d456d42ff2385a34c4f89cca394e",
+    "url": "/soccer-club-spa/index.html"
+  }
+];
